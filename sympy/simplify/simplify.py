aa12aa4d6d39c698143c1090676c20fb8a8530b1
Peter Schmidt <peter@peterjs.com>
2015-06-02 17:52:40 +1000

    Might not need this one? If we do it's ~20% of the total time taken in one specific test, so worth spending time looking for optimisations. 3.361s -> 2.637s

--- sympy/simplify/simplify.py
+++ sympy/simplify/simplify.py
@@ -4354,7 +4354,7 @@ def _futrig(e, **kwargs):
         [identity, lambda x: _eapply(_mexpand, x, trigs)],
         TRmorrie,
         TR10i,  # sin-cos products > sin-cos of sums
-        [identity, TR8],  # sin-cos products -> sin-cos of sums
+        # [identity, TR8],  # sin-cos products -> sin-cos of sums
         [identity, lambda x: TR2i(TR2(x))],  # tan -> sin-cos -> tan
         [
             lambda x: _eapply(expand_mul, TR5(x), trigs),
